81fc4d8f0379bffd307577152f72bc396ea36018
consiler <mtponte@gmail.com>
2014-08-08 00:54:17 -0700

    added the stuff

---
+++ EventEmitter.js
@@ -0,0 +1,118 @@
+(function () {
+  'use strict';
+
+  function EventEmitter() {
+    console.log('this gets run');
+    this._events = {};
+    this._maxListeners = 10;
+  }
+
+  EventEmitter.listenerCount = function(emitter, event) {
+    if (emitter._events.hasOwnProperty(event)) {
+      return emitter._events[event].length;
+    }
+    return 0;
+  };
+
+  EventEmitter.prototype.on = function (event, listener) {
+    if (typeof listener !== 'function') {
+      throw new TypeError('listener must be a function');
+    }
+    if (this._events.hasOwnProperty(event)) {
+      this._events[event].push(listener);
+    } else {
+      this._events[event] = [listener];
+    }
+    if (this._events[event].length === this._maxListeners + 1) {
+      console.error('EventEmitter: Possible memory leak. More than the maximum number of listeners (' + this._maxListeners + ') added for event \'' + event + '\'. Increase this limit using emitter.setMaxListeners(n).');
+      console.trace();
+    }
+    this.emit('newListener');
+    return this;
+  };
+
+  EventEmitter.prototype.addEventListener = EventEmitter.prototype.on;
+
+  EventEmitter.prototype.once = function (event, listener) {
+    if (typeof listener !== 'function') {
+      throw new TypeError('listener must be a function');
+    }
+    this.on(event, function oncewrap() {
+      this.removeListener(event, oncewrap);
+      listener.apply(this, arguments);
+    });
+  };
+
+  EventEmitter.prototype.removeListener = function (event, listener) {
+    if (typeof listener !== 'function') {
+      throw new TypeError('listener must be a function');
+    }
+    if (!this._events.hasOwnProperty(event)) {
+      return this;
+    }
+    this.emit('removeListener');
+    var i;
+    if (this._events[event].length === 1) {
+      return this.removeAllListeners(event);
+    }
+    if (this._events[event] && (i = this._events[event].indexOf(listener)) !== -1) {
+      this._events[event].splice(i, 1);
+    }
+    return this;
+  };
+
+  EventEmitter.prototype.removeAllListeners = function (event) {
+    var n, i, that = this;
+    if (event === undefined) {
+      var keys = Object.keys(this._events).forEach(function (event) {
+        if (event !== 'removeListener') {
+          that.removeAllListeners(event);
+        }
+      });
+      if (this._events.hasOwnProperty('removeListener')) {
+        delete this._events['removeListener'];
+      }
+      return this;
+    }
+    if (!this._events.hasOwnProperty(event)) {
+      return this;
+    }
+    n = this._events[event].length;
+    for (i = 0; i < n; i++) {
+      this.emit('removeListener');
+    }
+    delete this._events[event];
+    return this;
+  };
+
+  EventEmitter.prototype.setMaxListeners = function (n) {
+    this._maxListeners = n;
+  };
+
+  EventEmitter.prototype.listeners = function (event) {
+    if (this._events.hasOwnProperty(event)) {
+      return this._events[event];
+    } else {
+      return [];
+    }
+  };
+
+  EventEmitter.prototype.emit = function (event) {
+    var that = this, additionalArguments = Array.prototype.slice.call(arguments, 1);
+    if (!this._events.hasOwnProperty(event)) {
+      if (event === 'error') {
+        throw new Error(arguments[1] || 'Uncaught error event. Attach an \'error\' event listener to handle this case.');
+      }
+      return false;
+    }
+    this._events[event].forEach(function (f) {
+      f.apply(that, additionalArguments);
+    });
+    return true;
+  };
+
+  module.exports = EventEmitter;
+  if (window) {
+    window.EventEmitter = EventEmitter;
+  }
+}());
---
+++ EventEmitter_test.js
@@ -0,0 +1,89 @@
+//var EventEmitter = require('events').EventEmitter;
+var EventEmitter = require('./EventEmitter.js');
+var assert = require('assert');
+
+(function () {
+  'use strict';
+
+  var e = new EventEmitter();
+
+  function isPrime(n) {
+    if (n % 2 === 0) {
+      return false;
+    }
+    var i, sqrtn = ~~(Math.sqrt(n) + 1);
+    for (i = 3; i < sqrtn; i += 2) {
+      if (n % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  var nErrors = 0;
+  e.on('error', function (err) {
+    nErrors++;
+  });
+  e.emit('error');
+  e.emit('error');
+  assert.equal(nErrors, 2);
+
+  var primeProduct = 1;
+  e.on('prime', function () {
+    var i;
+    for (i = 0; i < arguments.length; i++) {
+      primeProduct *= arguments[i];
+    }
+  });
+  e.emit('prime');
+  e.emit('prime', 2);
+  e.emit('prime', 3, 5);
+  e.emit('prime', 7, 11, 13);
+  assert.equal(primeProduct, 2 * 3 * 5 * 7 * 11 * 13);
+
+  //Test if .listeners and EventEmitter.listenerCount work
+  Object.keys(e._events).forEach(function (k) {
+    assert.equal(e.listeners(k).length, EventEmitter.listenerCount(e, k));
+  });
+
+  var livesLived = 0;
+  function liveLife() {
+    livesLived++;
+  }
+  //add some junk listeners so we know once will remove our listener, not just any listener
+  var i;
+  for (i = 0; i < 10; i++) {
+    e.on('live', function () {});
+  }
+  //Increase max listeners and add the real listener. This will crash if setMaxListeners fails
+  e.setMaxListeners(11);
+  e.once('live', liveLife);
+  //this should trigger the warning that we are adding more than 10
+  e.emit('live');
+  e.emit('live');
+  e.emit('live');
+  //once should trigger a callback exactly one time and then delete the listener
+  assert.equal(livesLived, 1);
+  assert.equal(e.listeners('live').indexOf(liveLife), -1);
+
+  var nListenersToRemove = Object.keys(e._events).map(function (k) {
+    if (k === 'removeListener') {
+      return 0;
+    }
+    var L = e.listeners(k);
+    if (Array.isArray(L)) {
+      return L.length;
+    }
+    return 1;
+  }).reduce(function (x, y) {
+    return x + y;
+  });
+  var nRemovedListeners = 0;
+  e.on('removeListener', function () {
+    nRemovedListeners++;
+  });
+  e.removeAllListeners();
+  assert.equal(nRemovedListeners, nListenersToRemove);
+
+  console.log('All Tests Passed.');
+}());
